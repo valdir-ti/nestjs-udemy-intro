63825e8f8b78bdba65d7dc83ccc36f612d75a959
Valdir Silva <valdir.ti@gmail.com>
2025-03-13 15:44:18 -0300

    feat: create the post method to add a new recado

--- src/recados/recados.controller.ts
+++ src/recados/recados.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('recados')
 export class RecadosController {
@@ -10,5 +10,10 @@ export class RecadosController {
     @Get(':id')    
     findOne(@Param('id') id: string) {        
         return `Retorna somente um recado ${id}`
+    }
+
+    @Post()
+    async create(@Body() body: any) {
+        return `Cria um novo recado : ${body.message}`
     }
 }
