11f16b6113ae0e491d0e560fab6b6e115b38314f
Valdir Silva <valdir.ti@gmail.com>
2025-03-13 16:23:27 -0300

    feat: create delete, patch and put route

--- src/recados/recados.controller.ts
+++ src/recados/recados.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put} from '@nestjs/common';
 
 @Controller('recados')
 export class RecadosController {
@@ -15,5 +15,15 @@ export class RecadosController {
     @Post()
     async create(@Body() body: any) {
         return `Cria um novo recado : ${body.message}`
+    }
+    
+    @Put(':id')
+    async update(@Param('id') id: string, @Body() body: any) {
+        return `Atualiza o recado: ${id} com a nova mensagem: '${body.message}'`
+    }
+
+    @Delete(':id')
+    async delete(@Param('id') id: string) {
+        return `Deleta o recado: ${id}`
     }
 }
